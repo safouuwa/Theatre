0129e36a129dc86df53ceab4f3069f6cd294a03c
linkverk <yegorgudz@gmail.com>
2024-12-30 12:49:08 +0100

    Update 2.3
    Updated met de login knop voor log in

--- theatre/src/components/HomePage.tsx
+++ theatre/src/components/HomePage.tsx
@@ -23,7 +23,10 @@ const HomePage: React.FC = () => {
     useEffect(() => {
         axios.get<TheatreShow[]>('http://localhost:5097/api/v1/TheatreShow')
             .then(response => {
-                setShows(response.data);
+                const futureShows = response.data.filter(show => 
+                    new Date(show.theatreShowDates[0].dateAndTime) > new Date()
+                );
+                setShows(futureShows);
             })
             .catch(error => {
                 console.error('Error fetching shows:', error);
@@ -34,16 +37,23 @@ const HomePage: React.FC = () => {
         navigate(`/show/${id}`);
     };
 
+    const handleLoginClick = () => {
+        navigate('/login');
+    };
+
     return (
-        <div className="homepage-container">
-            <h1>Available Shows</h1>
-            <ul>
-                {shows.map(show => (
-                    <li key={show.theatreShowId} onClick={() => handleShowClick(show.theatreShowId)}>
-                        {show.title} - {new Date(show.theatreShowDates[0].dateAndTime).toLocaleString()}
-                    </li>
-                ))}
-            </ul>
+        <div>
+            <button className="login-button" onClick={handleLoginClick}>Login</button>
+            <div className="homepage-container">
+                <h1>Available Shows</h1>
+                <ul>
+                    {shows.map(show => (
+                        <li key={show.theatreShowId} onClick={() => handleShowClick(show.theatreShowId)}>
+                            {show.title} - {new Date(show.theatreShowDates[0].dateAndTime).toLocaleString()}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 };
