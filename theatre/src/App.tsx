5c3fa77a91ef456f16aea9effa958f7b7e6a6f7f
linkverk <yegorgudz@gmail.com>
2025-01-02 09:47:12 +0100

    Remake 2.3
    Hier is remake van code die had probleem met branch not found

--- theatre/src/App.tsx
+++ theatre/src/App.tsx
@@ -4,12 +4,16 @@ import Login from './components/Login.tsx';
 import Dashboard from './components/Dashboard.tsx';
 import TheatreShows from './components/TheatreShows.tsx';
 import Reservations from './components/Reservations.tsx';
+import HomePage from './components/HomePage.tsx';
+import ShowDetails from './components/ShowDetails.tsx'; // Corrected import
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/show/:id" element={<ShowDetails />} />
+        <Route path="/Login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/theatre-shows" element={<TheatreShows />} />
         <Route path="/reservations" element={<Reservations />} />
